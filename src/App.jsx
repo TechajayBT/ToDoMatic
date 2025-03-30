554d68046558d3e09c77bab8b8051bad15d2629d
Ajay Singh Bartwal <142952841+TechajayBT@users.noreply.github.com>
2025-03-30 21:55:26 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { nanoid } from "nanoid";
+import { useState } from "react";
+import FilterButton from "./Components/FilterButton";
+import Form from "./Components/Form";
+import Todo from "./Components/ToDo";
+import "./App.css";
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) =>
+      id === task.id ? { ...task, name: newName } : task
+    );
+    setTasks(editedTaskList);
+  }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) =>
+      id === task.id ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter]) // ✅ Now filter is properly defined
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton key={name} name={name} setFilter={setFilter} />
+  ));
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">{filterList}</div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
